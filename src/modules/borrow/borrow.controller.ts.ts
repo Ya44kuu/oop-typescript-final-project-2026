08d6cd05c400d1b9cf44d740530390f6344f9d5c
Ya44kuu <mr.sirisart1999@gmail.com>
2026-03-02 00:04:06 +0700

    struct

--- src/modules/borrow/borrow.controller.ts.ts
+++ src/modules/borrow/borrow.controller.ts.ts
@@ -1,1 +1,34 @@
-console.log("projest")
+import { Controller, Get, Post, Patch, Delete, Param, Body } from '@nestjs/common';
+import { BorrowService } from './borrow.service';
+import { CreateBorrowDto } from './dto/create-borrow.dto';
+
+@Controller('borrows')
+export class BorrowController {
+
+  constructor(private readonly borrowService: BorrowService) {}
+
+  @Get()
+  findAll() {
+    return this.borrowService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.borrowService.findOne(Number(id));
+  }
+
+  @Post()
+  create(@Body() dto: CreateBorrowDto) {
+    return this.borrowService.create(dto);
+  }
+
+  @Patch(':id/return')
+  returnBook(@Param('id') id: string) {
+    return this.borrowService.returnBook(Number(id));
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.borrowService.remove(Number(id));
+  }
+}
