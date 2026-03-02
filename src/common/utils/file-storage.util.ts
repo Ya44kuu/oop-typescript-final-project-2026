89466b44a56717b1322b3df0eb36737ae0c9bbc3
banana <watnirun03@gmail.com>
2026-03-02 14:27:34 +0700

    banana

---
+++ src/common/utils/file-storage.util.ts
@@ -0,0 +1,26 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+export class FileStorageUtil {
+  static readData<T>(fileName: string): T[] {
+    const filePath = path.resolve(__dirname, `../../../data/${fileName}`);
+    
+    if (!fs.existsSync(filePath)) {
+      return [];
+    }
+
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    return JSON.parse(fileContent) as T[];
+  }
+
+  static writeData<T>(fileName: string, data: T[]): void {
+    const dirPath = path.resolve(__dirname, '../../../data');
+    const filePath = path.join(dirPath, fileName);
+    
+    if (!fs.existsSync(dirPath)) {
+      fs.mkdirSync(dirPath, { recursive: true });
+    }
+    
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf-8');
+  }
+}
